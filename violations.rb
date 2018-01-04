3ed5cac0ce2f1e83504c6d6e800187b6a1315fe5
nico24687 <theonlynico@hotmail.com>
2018-01-04 10:06:22 -0700

    final commit

--- violations.rb
+++ violations.rb
@@ -1,17 +1,20 @@
 require 'pry'
 require 'csv'
 
-unique_count = {}
+unique_count = Hash.new(0)
+
 
 
 CSV.foreach('violations.csv', :headers => true) do |row|
-  unique_count[row[5]] ||= 0
   unique_count[row[5]] += 1
+  dates = row["violation_date"]
 end
 
 
   
 unique_count.each do |value, count|
   puts "-------" * 15 
-  puts "#{value}: #{count} time/s, first occured on: , last occured on: "
+  puts "#{value}: #{count} time/s, first occured on: #{} , last occured on: #{} "
 end
+
+
